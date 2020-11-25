a7a28d507a58b2eb32f75d694e9c3a81284c98c5
smad2 <salvamadrid303@gmail.com>
2020-11-25 15:51:53 +0000

    helpdesk Salva Madrid 2

--- helpdesk_salvamad/models/helpdesk.py
+++ helpdesk_salvamad/models/helpdesk.py
@@ -1,3 +1,5 @@
+
+
 from odoo import models, fields
 
 class HelpdeskTicket(models.Model):
@@ -5,10 +7,40 @@ class HelpdeskTicket(models.Model):
 	_description = "Helpdesk Ticket"
 	
 	name = fields.Char(
-		string='Name')	
+		string='Name',
+		required=True)	
+		
 	description = fields.Text(
 		string='Description')
 	date = fields.Date(
 		string='Date')
+	#Estado [Nuevo, Asignado, En proceso, Pendiente, Resuelto, Cancelado], que por defecto sea Nuevo
+	state = fields.Selection(
+		[('new', 'New'),
+		('assigned', 'Assigned'),
+		('progress', 'Progress'),
+		('waiting', 'Waiting'),
+		('done', 'Done'),
+		('cancel', 'Cancel')],
+		string='State',
+		default='new')
+		
+	#Tiempo dedicado (en horas)
+	dedicated_time = fields.Float(
+	string='Time')
 	
+	#Asignado (tipo check)
+	assigned = fields.Boolean(
+	string='Assigned',
+	readonly=True)
 	
+	#Fecha límite
+	date_due = fields.Date(
+	string='Date Due')
+	
+	#Acción correctiva (html)
+	corrective_action = fields.Html(
+	help='Detail of corrective action after this issue')
+	#Acción preventiva (html)
+	preventive_action = fields.Html(
+	help='Detail of preventive action after this issue')
